fd44a2cd38f360ee93b195047308eff489e189d7
quynhcooll <87225264+quynhcooll@users.noreply.github.com>
2021-07-11 14:54:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
+    introDesc: `Trái đất vốn lạ thưng
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
